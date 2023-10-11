cefccd7fe5cb49fc133ae39a5328d92584315a07
Shiimymy <>
2023-10-11 13:30:12 +0000

    Add showTurns func and test

--- scripts/game.js
+++ scripts/game.js
@@ -16,7 +16,7 @@ function newGame() {
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
-    //showTurns();
+    showTurns();
 }
 
 function showScore() {
@@ -30,4 +30,18 @@ function lightsOn(circ) {
     }, 400);
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn };
+function showTurns() {
+    /* Setting a interval turning the lightsOn,
+    incrementing the game turnNumber,
+    and then turning them off again.*/
+    game.turnNumber = 0;
+    let turns = setInterval(function () {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
